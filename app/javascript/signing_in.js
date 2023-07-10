056e41e3991a583e0d5ca5566783fb2c20f6237c
AndieAtWork <andieperezchavez@gmail.com>
2023-07-10 15:21:42 -0400

    antes de agregar validator

--- app/javascript/signing_in.js
+++ app/javascript/signing_in.js
@@ -1,1 +1,26 @@
 console.log("Signing in validator is running!")
+
+//create_session_form
+//create_user_form
+
+$( document ).ready(function() {
+	$('#create_session_form').bootstrapValidator({
+		fields: {
+			username: {
+				message: 'El nombre de usuario no es valido',
+				validators: {
+					notEmpty: {
+						message: 'El nombre de usuario es  necesario'
+					}
+				}
+			},
+			password: {
+				validators: {
+					notEmpty: {
+						message: 'La contraseña es necesaria'
+					}
+				}
+			},
+		}
+	});
+});
