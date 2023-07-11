1d51291efb1f0ded77bec6e309dc3d5439c2e023
AndieAtWork <andieperezchavez@gmail.com>
2023-07-11 01:21:56 -0400

    normal validators working fine

--- app/javascript/custom_validator.js
+++ app/javascript/custom_validator.js
@@ -16,9 +16,10 @@ CustomValidator.create_custom_validator = function(form_id, input_class, input_v
             var validators = input_validations[input.id]
 
             var error_message = '';
-            for  (var validator of validators){
-                var result =  validator[0](text_value, validator[1])
-                console.log(result);
+            for (var validator of validators){
+                var validator_function = validator["validator"]
+                var parameters = validator["params"]
+                var result =  validator_function(text_value, parameters)
                 if (!result[0]){
                     error_message += result[1] + "\n";
                 }
@@ -51,8 +52,8 @@ CustomValidator.create_custom_validator = function(form_id, input_class, input_v
 }
 
 CustomValidator.length_validator = function(text, params){
-    var min_length = params[0];
-    var max_length = params[1];
+    var min_length = params["min_length"];
+    var max_length = params["max_length"];
     if (text.length > max_length){
         return [false, "The length must be between " +  String(min_length) +" and " + String(max_length) + " characters."]
     }else if (text.length < min_length){
@@ -80,15 +81,63 @@ CustomValidator.password_validator = function(text){
     if (has_lower_letters && has_upper_letters && has_numbers){
         return [true];
     }else{
-        return [false, "Your password has to contain at least one lowercase character, one uppercase character and a number."];
+        return [false, "Your password must contain at least one lowercase character, one uppercase character and a number."];
     }
 }
 
-CustomValidator.identical_value = function(text, identical_id){
+CustomValidator.identical_value = function(text, params){
+    var identical_id = params["identical_id"];
     var identical = document.getElementById(identical_id).value;
     if (text == identical){
         return [true];
     }else{
         return [false, "The two values are not identical."];
     }
+}
+
+CustomValidator.not_empty = function(text, params){
+    var empty = false;
+    if (text.length == 0){
+        empty = true;
+    }else{
+        empty = true;
+        for (var character of text){
+            if (character != " "){
+                empty = false;
+                break;
+            }
+        }
+    }
+    return [!empty, params["message"]]
+}
+
+CustomValidator.name_validator = function(text, params){
+    if (text.length == 0){
+        return [true]
+    }
+    var spaces = params["spaces"]
+    var only_numbers = true;
+    for (var character of text){
+        if (/^\d$/.test(character)){
+        }
+        else if (/^[a-z]$/.test(character)){
+            only_numbers = false;
+        }
+        else if (/^[A-Z]$/.test(character)){
+            only_numbers = false;
+        }
+        else if (spaces && (character == " " || character == "-")){ 
+        }
+        else{
+            if (spaces){
+                return [false, "Can only contain letters, numbers, spaces and hyphens"]
+            }else{
+                return [false, "Can only contain letters and numbers"]
+            }
+        }
+    }
+    if (only_numbers){
+        return [false, "Name cannot contain only numbers."]
+    }
+    return [true]
 }
--- app/javascript/signing_in.js
+++ app/javascript/signing_in.js
@@ -1,20 +1,41 @@
-console.log("Signing in validator is running!")
+console.log("Signing in validator is ridentical_idunning!")
 
 //create_session_form
 //create_user_form
 
 if (document.getElementById("create_session_form")){
     console.log("signing in");
+
+    var input_validations = {
+        "input1": [
+            {"validator": CustomValidator.not_empty, "params": {"message": "Enter a username."}}
+        ],
+        "input2": [
+            {"validator": CustomValidator.not_empty, "params": {"message": "Enter a password."}}
+        ]
+    };
+    window.addEventListener('load', function() {
+        CustomValidator.create_custom_validator("create_session_form", "form-control-validated", input_validations)
+    }, false);
 }
 
 if (document.getElementById("create_user_form")){
     console.log("creating user");
 
     var input_validations = {
-        "input1": [[CustomValidator.length_validator, [7,15]]],
-        "input2": [[CustomValidator.length_validator, [8,15]],[CustomValidator.password_validator]],
-        "input3": [[CustomValidator.identical_value, ["input2"]]]
-    }
+        "input1": [
+            {"validator": CustomValidator.length_validator, "params": {"min_length": 7, "max_length": 15}},
+            {"validator": CustomValidator.name_validator, "params": {"spaces": true}}
+        ],
+        "input2": [
+            {"validator": CustomValidator.length_validator, "params": {"min_length": 8, "max_length": 15}},
+            {"validator": CustomValidator.password_validator}
+        ],
+        "input3": [
+            {"validator": CustomValidator.identical_value, "params": {"identical_id": "input2"}},
+            {"validator": CustomValidator.not_empty, "params": {"message": "Field cannot be left empty."}}
+        ]
+    };
 
     window.addEventListener('load', function() {
         CustomValidator.create_custom_validator("create_user_form", "form-control-validated", input_validations)
